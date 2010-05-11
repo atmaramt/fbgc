c39a94ffe29f7a4d4771f78ff46d45dfa6d814f0
ckilit <ckilit@f4f655db-543d-fab3-b180-db48ae8f8c2a>
2010-05-11 12:25:02 +0000

    fb callback servlet added

---
+++ trunk/daemon/src/main/java/org/albino/FacebookCallbackHandler.java
@@ -0,0 +1,24 @@
+package org.albino;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class FacebookCallbackHandler extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+
+		System.out.println(request.getParameter("fb_sig_user"));
+		;
+
+	}
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
